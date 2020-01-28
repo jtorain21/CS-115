682f243e1fa3249a64584fb56d3d07add40cc1f0
Joshua Torain <josh@Joshuas-MacBook-Pro.local>
2020-01-27 21:52:50 -0500

    added C to F table

---
+++ Celsius_to_Fahrenheit_Table.py
@@ -0,0 +1,19 @@
+# Set Variables - celsius = 0
+# Set up the conversion of Celsius to Fahrenheit using the formula
+# Set the range of temperatures from 0 to 20
+# Use a while loop to iterate over each value 0 through 20.
+
+# Formula --> F = (9/5) * C + 32
+
+# create celsius variable. Make it zero since that's our starting temperature
+celsius = 0
+# Create a while loop to iterate over each value from 0 through 20. Print out the values
+# Set range of temperatures to be from 0 to 20 Celsius
+while 0 <= celsius <= 20:
+    # enter conversion formula.  changed (9/5) to 1.8 for simplicity
+    fahrenheit = (celsius * 1.8) + 32
+    # print out temperatures. Format fahrenheit so that it only displays one number after decimal
+    print('Celsius:', celsius, 'Fahrenheit:', format(fahrenheit, '.1f'))
+    # set celsius variable to count by one, to repeat loop until 20
+    celsius = celsius + 1
+# While loop should terminate once it reaches 20 and it is "no longer true"
